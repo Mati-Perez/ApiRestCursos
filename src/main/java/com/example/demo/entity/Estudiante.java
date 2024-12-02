00269363819bf56229ca40441d1080c730e105aa
Matias Perez <matiperez881mail.com>
2024-12-02 16:49:00 -0300

    Implement ORM to all entities: curso - estudiante - profesor

--- src/main/java/com/example/demo/entity/Estudiante.java
+++ src/main/java/com/example/demo/entity/Estudiante.java
@@ -2,6 +2,13 @@ package com.example.demo.entity;
 
 import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +16,65 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name="estudiantes")
 public class Estudiante {
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="est_id")
 	private Long id;
+	@Column(name="est_nombre")
 	private String nombre;
+	@Column(name="est_email")
 	private String email;
+	@ManyToMany(mappedBy="estudiantes")
 	private List<Curso> cursos;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/example/demo/entity/Profesor.java
+++ src/main/java/com/example/demo/entity/Profesor.java
@@ -2,6 +2,14 @@ package com.example.demo.entity;
 
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +17,16 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name="profesores")
 public class Profesor {
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="prof_id")
 	private Long id;
+	@Column(name="prof_nombre")
 	private String nombre;
+	@Column(name="prof_especialidad")
 	private String especialidad;
+	@OneToMany(mappedBy="profesor", cascade=CascadeType.ALL, orphanRemoval=true)
 	private List<Curso> cursos;
 }
